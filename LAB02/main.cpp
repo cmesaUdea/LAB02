eb0cd32bc580f30d00159bb430197d8989294307
cmesaUdea <carlos.mesar@udea.edu.co>
2023-09-19 10:51:03 -0500

    Se mejoro el problema de la maquina dispensadora de dinero

--- LAB02/main.cpp
+++ LAB02/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int opcion = 0;
+    int opcion = 1;
     cout<<"Bienvenido a la entrega del Laboratorio 2 de Informatica 2"<<endl;
     cout<<"Elaborado por: Carlos Andres Mesa Roldan, estudiante de Ing. Telecomunicaciones"<<endl;
     cout<<"______________________________________________________________________________________"<<endl;
@@ -29,15 +29,36 @@ int main()
     cout<<"______________________________________________________________________________________"<<endl;
     cout<<"Si ingresa una opcion invalida, saldra del programa."<<endl;
     cout<<"Ingrese la opcion: "<<endl;
-    cin >> opcion;
 
+    cin>>opcion;
 
-    switch(opcion){
+    switch (opcion) {
+    case 1:
+    // Problema 1.
 
-    case 1:{
+    /*Problema 1. Se necesita un programa que permita determinar la mínima combinación de billetes y monedas para
+    una cantidad de dinero determinada. Los billetes en circulación son de $50.000, $20.000, $10.000, $5.000, $2.000
+    y $1.000, y las monedas son de $500, $200, $100 y $50. Hacer un programa que entregue el número de billetes
+    y monedas de cada denominación para completar la cantidad deseada. Si por medio de los billetes y monedas
+    disponibles no se puede lograr la cantidad deseada, el sistema deberá decir lo que resta para lograrla. Use arreglos
+    y ciclos para realizar el programa.
+    Ejemplo: si se ingresa 47810, el programa debe imprimir:
+    50000 : 0
+    20000: 2
+    10000 : 0
+    5000: 1
+    2000 : 1
+    1000: 0
+    500 : 1
+    200: 1
+    100 : 1
+    50: 0
+    Faltante: 10 */
+
+    {
 
         int money = 0;
-        cout << "Ha elegido el problema 2" << endl;
+        cout << "Ha elegido el problema 1" << endl;
         cout << "Ingrese una cantidad de dinero:"<<endl;
         cin >> money;
 
@@ -56,8 +77,6 @@ int main()
         break;
     }
     }
-
-
 
     return 0;
 }
